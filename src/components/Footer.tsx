dcecd4eabd628b137c8cea145fe04163d1ee84d7
robinglto <robinsomedia@pm.me>
2024-02-17 12:24:39 -0400

    fixed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 export default function Footer() {
   const links =
-    "hover:text-gray-800 dark:hover:text-gray-300 hover:decoration-dashed hover:underline cursor-pointer transition duration-500 ease-in-out";
+    "hover:text-gray-800 dark:hover:text-gray-300 decoration-dashed underline cursor-pointer transition duration-500 ease-in-out underline-offset-4";
   return (
-    <div className="flex justify-center p-4 md:dark:bg-black dark:bg-black md:bg-gray-100 bg-gray-200 dark:text-white text-black">
+    <div className="flex justify-center p-4 md:dark:bg-black dark:bg-black md:bg-gray-100 bg-gray-200 dark:text-white text-black ">
       <div className="space-y-2">
         <p className="   mx-10 text-center  text-md">
           Built with{" "}
@@ -14,7 +14,7 @@ export default function Footer() {
           </Link>
           ,{" "}
           <Link className={links} href="https://tailwindcss.com/">
-            Tailwind
+            TailwindCSS
           </Link>{" "}
           and{" "}
           <Link className={links} href="https://vercel.com/">
@@ -22,17 +22,8 @@ export default function Footer() {
           </Link>{" "}
           by{" "}
           <Link className={links} href="https://robin.do">
-            Robin
+            robin.do
           </Link>{" "}
-          ({" "}
-          <Link className={links} href="https://twitter.com/robinglto_">
-            @robinglto_ on twitter
-          </Link>{" "}
-          ,{" "}
-          <Link className={links} href="https://github.com/robinglto">
-            @robinglto on github
-          </Link>{" "}
-          )
         </p>
       </div>
     </div>
