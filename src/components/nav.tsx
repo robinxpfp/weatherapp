0615855955c60a1df647c6e2ac0d47bff09615e1
elmoghost <ghostsln@proton.me>
2025-03-23 19:02:03 -0400

    add mode-toggle

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,5 +1,6 @@
 import { Clock3 } from "lucide-react";
 import { SearchForm } from "@/components/search-form";
+import { ModeToggle } from "./mode-toggle";
 
 type SearchFormProps = {
   city: string;
@@ -10,9 +11,7 @@ export function Nav({ city, setCity }: SearchFormProps) {
   return (
     <header className="flex items-center justify-between p-4 sm:p-8 sm:mb-4 mb-12">
       <div className="flex items-center gap-2 font-semibold">
-        <div className="w-8 h-8 rounded-full  flex items-center justify-center">
-          <Clock3 />
-        </div>
+        <ModeToggle />
         <span>TimeSpot</span>
       </div>
       <SearchForm city={city} setCity={setCity} />
