3b8d2a9100c559264e19c9a1ba35f867a5b02f5e
elmoghost <ghostsln@proton.me>
2025-03-23 19:30:14 -0400

    usign input from shadcn

--- src/components/search-form.tsx
+++ src/components/search-form.tsx
@@ -1,4 +1,5 @@
 import { Search } from "lucide-react";
+import { Input } from "./ui/input";
 
 type SearchFormProps = {
   city: string;
@@ -10,8 +11,8 @@ export function SearchForm({ city, setCity }: SearchFormProps) {
     <form onSubmit={(e) => e.preventDefault()}>
       <div className="relative">
         <Search className="absolute left-3 top-1/2 transform -translate-y-1/2  w-4 h-4" />
-        <input
-          className="rounded-full border border-gray-200 pl-10 pr-4 py-2 w-[200px] focus:outline-none focus:ring-2 focus:ring-gray-200 text-sm"
+        <Input
+          className="pl-10 rounded-3xl"
           type="text"
           placeholder="Search location by city"
           value={city}
