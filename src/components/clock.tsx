857e4d7c51183ab8890e3eae94a0f7c5d7b1ad46
elmoghost <ghostsln@proton.me>
2025-03-23 17:25:03 -0400

    add am and pm text

--- src/components/clock.tsx
+++ src/components/clock.tsx
@@ -9,14 +9,18 @@ type ClockProps = {
 
 export function Clock({ is24Hour, setIs24Hour }: ClockProps) {
   const [time, setTime] = useState<string>("");
+  const [period, setPeriod] = useState<string>("");
 
   useEffect(() => {
     const updateTime = () => {
       const now = new Date();
-      const hours = is24Hour ? now.getHours() : now.getHours() % 12 || 12;
+      const hours24 = now.getHours();
+      const hours = is24Hour ? hours24 : hours24 % 12 || 12;
       const minutes = String(now.getMinutes()).padStart(2, "0");
       const seconds = String(now.getSeconds()).padStart(2, "0");
+      const periodIndicator = !is24Hour ? (hours24 < 12 ? "AM" : "PM") : "";
       setTime(`${String(hours).padStart(2, "0")}:${minutes}:${seconds}`);
+      setPeriod(periodIndicator);
     };
 
     updateTime();
@@ -26,10 +30,13 @@ export function Clock({ is24Hour, setIs24Hour }: ClockProps) {
 
   return (
     <div className="flex flex-col items-center mb-6">
-      <span className="text-7xl md:text-8xl font-bold tracking-tighter mb-8">
-        {time}
-      </span>
-      <div className="flex gap-2">
+      <div className="flex items-baseline gap-2">
+        <span className="text-7xl md:text-8xl font-bold tracking-tighter">
+          {time}
+        </span>
+        <span className="text-2xl md:text-3xl font-bold">{period}</span>
+      </div>
+      <div className="flex gap-2 mt-8">
         <button
           onClick={() => setIs24Hour(false)}
           className={`px-4 py-1 rounded-full text-sm ${
